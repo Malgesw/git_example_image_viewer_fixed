3c0659aded3bbf19934a8d42e645fa01c8697e79
kaneki <niccolo.malgeri@stud.unifi.it>
2022-04-11 19:50:38 +0200

    Added program version code

---
+++ E1_git_example_image_viewer_exercise/version.h
@@ -0,0 +1,12 @@
+//
+// Created by kaneki on 11/04/22.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+static const std::string VERSION = "V1.0"
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
